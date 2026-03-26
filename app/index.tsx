dd8becc9fa5c3ce0d76cd775b5b4caf30bdc4dd2
Dinuka_Lakmal <89435060+Dinuka7084@users.noreply.github.com>
2026-03-27 02:36:40 +0530

    nativewind setup successfully

--- app/index.tsx
+++ app/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
       <Link href="/about"><Text>Go to About Page</Text></Link>
     </View>
   );
